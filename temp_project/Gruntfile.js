3fe769c82713b3952df98b17979d7c46330b49b5
post4me77 <post4me77@gmail.com>
2017-02-17 21:56:59 +0200

    temp_project
    My first temp project

---
+++ temp_project/Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt){
+grunt.initConfig({
+  concat: {
+    js: {
+      src: ['js/tableController.js'],
+      dest: 'build/js/script.js',
+    },
+    css: {
+      src: ['css/1.css'],
+      dest: 'build/css/styles.css',
+    },
+  },
+  watch: {
+    js: {
+      files: ['js/**/*.js'],
+      tasks: ['concat:js'],
+    },
+    css: {
+      files: ['css/**/*.css'],
+      tasks: ['concat:css'],
+    },
+  },
+});
+
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-watch');
+};
