8e2eebd2d7e025a0234b9044656b098f7a553a68
Vladimir Tkach <vtkach@google.com>
2017-02-19 11:27:44 +0200

    remove node_modules

--- temp_project/Gruntfile.js
+++ temp_project/Gruntfile.js
@@ -1,27 +1,27 @@
 module.exports = function(grunt){
-grunt.initConfig({
-  concat: {
-    js: {
-      src: ['js/tableController.js'],
-      dest: 'build/js/script.js',
+  grunt.initConfig({
+    concat: {
+      js: {
+        src: ['js/tableController.js'],
+        dest: 'build/js/script.js',
+      },
+      css: {
+        src: ['css/1.css'],
+        dest: 'build/css/styles.css',
+      },
     },
-    css: {
-      src: ['css/1.css'],
-      dest: 'build/css/styles.css',
+    watch: {
+      js: {
+        files: ['js/**/*.js'],
+        tasks: ['concat:js'],
+      },
+      css: {
+        files: ['css/**/*.css'],
+        tasks: ['concat:css'],
+      },
     },
-  },
-  watch: {
-    js: {
-      files: ['js/**/*.js'],
-      tasks: ['concat:js'],
-    },
-    css: {
-      files: ['css/**/*.css'],
-      tasks: ['concat:css'],
-    },
-  },
-});
+  });
 
-grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 };
